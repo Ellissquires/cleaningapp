8db906f079f769089a5f656cfceeff7e116ec715
Ellissquires <ellis.squires@gmail.com>
2016-09-05 16:00:40 +0100

    updated index

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -26,21 +26,10 @@ class HomeController extends Controller
 
     public function index()
     {
-        return view('home');
+        "it works";
 
 
 
     }
 
-    public function ViewUser(){
-      if (auth2.isSignedIn.get()) {
-        var profile = auth2.currentUser.get().getBasicProfile();
-        console.log('ID: ' + profile.getId());
-        console.log('Full Name: ' + profile.getName());
-        console.log('Given Name: ' + profile.getGivenName());
-        console.log('Family Name: ' + profile.getFamilyName());
-        console.log('Image URL: ' + profile.getImageUrl());
-        console.log('Email: ' + profile.getEmail());
-      }
-    }
 }
